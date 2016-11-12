f7acc4f271b1aa2d1c8275bc9361696340f022b9
VoraPranay <ppvora131@gmail.com>
2016-11-13 00:26:07 +0530

    [imad-console] Updates article.js

--- article.js
+++ article.js
@@ -1,74 +1,27 @@
-body {
-    font-family: sans-serif;
-    background-color: lightgrey;
-    margin-top: 75px;
-}
-
-.center {
-    text-align: center;
-}
-
-.text-big {
-    font-size: 300%;
-}
-
-.bold {
-    font-weight: bold;
-}
-
-.img-medium {
-    height: 200px;
-}
 
 
-body {
-    background-color:#bf8040;
-    color:#ffefaa;
-    font-family:Serif,Arial;
-}
-#Cakes{
-padding:10px;
-   background-color:#996633; 
-}
-table {
-background-color:#bf8040;
-border-collapse:collapse;
-border-color:#996633;
-}
-#Header{
-   background-color:#996633; 
-   padding:10px;
-}
-#ContactUs{
-   background-color:#996633;
-   padding:10px;
-   position:relative ;
-}
-#Copyright{
-font-family:Arial;
-   background-color:#996633; 
-   padding:1px;
-}.container {
-     max-width: 800px;
-     margin: 0 auto;
-     color: #4a4a4a;
-     font-family: sans-serif;
-     padding-left: 20px;
-     padding-right: 20px;
- }
+// Counter code
+ var button = document.getElementById('counter');
  
- .comment {
-     margin-left: 10px;
-     padding-bottom: 10px;
-     border-bottom: 1px solid #ccc;
- }
+ button.onclick = function () {
  
-  .commenter {
-      font-style: italic;
-      font-size: 0.8em;
-} 
-}
-.comment_form {
-    margin-left: 10px;
-    padding-bottom: 10px;    border-bottom: 1px solid #ccc;
- +}
+     // Create a request object
+     var request = new XMLHttpRequest();
+     
+     // Capture the response and store it in a variable
+     request.onreadystatechange = function () {
+       if (request.readyState === XMLHttpRequest.DONE) {
+           // Take some action
+           if (request.status === 200) {
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();          
+           }
+       }  
+       // Not done yet
+     };
+     
+     // Make the request
+     request.open('GET', 'http://VoraPranay.imad.hasura-app.io/counter', true);
+     request.send(null);
+
