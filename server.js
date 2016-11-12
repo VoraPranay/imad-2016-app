b58496c9eaeca4fb4b8c8ff5c80c5675b5a75ef3
VoraPranay <ppvora131@gmail.com>
2016-11-12 16:49:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,18 +3,10 @@ var express = require('express');
  var path = require('path');
  var Pool = require('pg').Pool;
  
- var config = {
-     user: 'coco98',
-     database: 'coco98',
-     host: 'db.imad.hasura-app.io',
-     port: '5432',
-     password: process.env.DB_PASSWORD
- };
- 
+
   var app = express();
   app.use(morgan('combined'));
   
- -
 var articles = {
  -    'article-one': {
  -      title: 'Article One | Tanmai Gopal',
@@ -24,22 +16,14 @@ var articles = {
  -          <p>
  -              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
  -          </p>
- -          <p>
- -              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
- -          </p>
- -          <p>
- -              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
- -          </p>`
+ -         
  -    },
  -    'article-two': {
  -      title: 'Article Two | Tanmai Gopal',
  -      heading: 'Article Two',
  -      date: 'Sep 10, 2016',
- -      content: `
- -          <p>
- -              This is the content for my second article.
- -          </p>`
- -    },
+ -      content: `   This is the content for my second article. 
+ -    }
  -    'article-three': {
  -      title: 'Article Three | Tanmai Gopal',
  -      heading: 'Article Three',
@@ -51,94 +35,7 @@ var articles = {
  -    }
  -};
  -
-  function createTemplate (data) {
-      var title = data.title;
-      var date = data.date;
-     var heading = data.heading;
-     var content = data.content;
-     
-     var htmlTemplate = `
-     <html>
-       <head>
-           <title>
-               ${title}
-           </title>
-           <meta name="viewport" content="width=device-width, initial-scale=1" />
-           <link href="/ui/style.css" rel="stylesheet" />
-       </head> 
-       <body>
-           <div class="container">
-               <div>
-                   <a href="/">Home</a>
-               </div>
-               <hr/>
-               <h3>
-                   ${heading}
-               </h3>
-               <div>
-                   ${date.toDateString()}
-               </div>
-               <div>
-                 ${content}
-               </div>
-           </div>
-       </body>
-     </html>
-     `;
-     return htmlTemplate;
- }
- 
- app.get('/', function (req, res) {
-   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
- });
- 
- var pool = new Pool(config);
- app.get('/test-db', function (req, res) {
-    // make a select request
-    // return a response with the results
-    pool.query('SELECT * FROM test', function (err, result) {
-       if (err) {
-           res.status(500).send(err.toString());
-       } else {
-           res.send(JSON.stringify(result.rows));
-       }
-    });
- });
- 
- var counter = 0;
- app.get('/counter', function (req, res) {
-    counter = counter + 1;
-    res.send(counter.toString());
- });
- 
- var names = [];
- app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
-   // Get the name from the request
-   var name = req.query.name;
-   
-   names.push(name);
-   // JSON: Javascript Object Notation
-   res.send(JSON.stringify(names));
-  });
   
-  app.get('/articles/:articleName', function (req, res) {
- -  // articleName == article-one
- -  // articles[articleName] == {} content object for article one
- -  
-    // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
-    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
-      if (err) {
-         res.status(500).send(err.toString());
-     } else {
-         if (result.rows.length === 0) {
-             res.status(404).send('Article not found');
-         } else {
-             var articleData = result.rows[0];
-             res.send(createTemplate(articleData));
-         }
-     }
-   });
- });
  
  app.get('/ui/style.css', function (req, res) {
    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
