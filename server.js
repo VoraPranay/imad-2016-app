31d9a018936e6ecd36f2f43e7b85515bd020d561
VoraPranay <ppvora131@gmail.com>
2016-11-12 16:29:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,42 +1,109 @@
-var express = require('express');
-var morgan = require('morgan');
-var path = require('path');
-
-var app = express();
-app.use(morgan('combined'));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-
-
-
-
-
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
-});
+ var express = require('express');
+ var morgan = require('morgan');
+ var path = require('path');
+ var Pool = require('pg').Pool;
+ 
+ var config = {
+     user: 'coco98',
+     database: 'coco98',
+     host: 'db.imad.hasura-app.io',
+     port: '5432',
+     password: process.env.DB_PASSWORD
+ };
+ 
+  var app = express();
+  app.use(morgan('combined'));
+  
+ -
+ -var articles = {
+ -    'article-one': {
+ -      title: 'Article One | Tanmai Gopal',
+ -      heading: 'Article One',
+ -      date: 'Sep 5, 2016',
+ -      content: `
+ -          <p>
+ -              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+ -          </p>
+ -          
+ -    }
+ -    'article-two': {
+ -      title: 'Article Two | Tanmai Gopal',
+ -      heading: 'Article Two',
+ -      date: 'Sep 10, 2016',
+ -      content: `
+ -          <p>
+ -              This is the content for my second article.
+ -          </p>
+ -    },
+ -    'article-three': {
+ -      title: 'Article Three | Tanmai Gopal',
+ -      heading: 'Article Three',
+ -      date: 'Sep 15, 2016',
+ -      content: `
+ -          <p>
+ -              This is the content for my third article.
+ -          </p>`
+ -    }
+ -};
+ -
+  function createTemplate (data) {
+      var title = data.title;
+      var date = data.date;
+ @@ -102,29 +65,26 @@
+       } else {
+           res.send(JSON.stringify(result.rows));
+       }
+    });
+ });
+ 
+ var counter = 0;
+ app.get('/counter', function (req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+ });
+ 
+ var names = [];
+ app.get('/submit-name', function(req, res) { // /submit-name?name=xxxx
+   // Get the name from the request
+   var name = req.query.name;
+   
+   names.push(name);
+   // JSON: Javascript Object Notation
+   res.send(JSON.stringify(names));
+  });
+  
+  app.get('/articles/:articleName', function (req, res) {
+ -  // articleName == article-one
+ -  // articles[articleName] == {} content object for article one
+ -  
+    // SELECT * FROM article WHERE title = '\'; DELETE WHERE a = \'asdf'
+    pool.query("SELECT * FROM article WHERE title = $1", [req.params.articleName], function (err, result) {
+      if (err) {
+         res.status(500).send(err.toString());
+     } else {
+         if (result.rows.length === 0) {
+             res.status(404).send('Article not found');
+         } else {
+             var articleData = result.rows[0];
+             res.send(createTemplate(articleData));
+         }
+     }
+   });
+ });
+ 
+ app.get('/ui/style.css', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ });
+ 
+ app.get('/ui/main.js', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+ });
+ 
+ app.get('/ui/madi.png', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+ });
+ 
+ var port = 8080; // Use 8080 for local development because you might already have apache running on 80
+ app.listen(8080, function () {
+   console.log(`IMAD course app listening on port ${port}!`);
+ });
