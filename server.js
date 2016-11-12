2db4d921d0fd3479e00019559c83184214ba6ae1
VoraPranay <ppvora131@gmail.com>
2016-11-12 16:40:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
- var express = require('express');
+var express = require('express');
  var morgan = require('morgan');
  var path = require('path');
  var Pool = require('pg').Pool;
@@ -24,8 +24,13 @@
  -          <p>
  -              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
  -          </p>
- -          
- -    }
+ -          <p>
+ -              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+ -          </p>
+ -          <p>
+ -              This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. This is the content for my first article. 
+ -          </p>`
+ -    },
  -    'article-two': {
  -      title: 'Article Two | Tanmai Gopal',
  -      heading: 'Article Two',
@@ -33,7 +38,7 @@
  -      content: `
  -          <p>
  -              This is the content for my second article.
- -          </p>
+ -          </p>`
  -    },
  -    'article-three': {
  -      title: 'Article Three | Tanmai Gopal',
@@ -49,7 +54,51 @@
   function createTemplate (data) {
       var title = data.title;
       var date = data.date;
- @@ -102,29 +65,26 @@
+     var heading = data.heading;
+     var content = data.content;
+     
+     var htmlTemplate = `
+     <html>
+       <head>
+           <title>
+               ${title}
+           </title>
+           <meta name="viewport" content="width=device-width, initial-scale=1" />
+           <link href="/ui/style.css" rel="stylesheet" />
+       </head> 
+       <body>
+           <div class="container">
+               <div>
+                   <a href="/">Home</a>
+               </div>
+               <hr/>
+               <h3>
+                   ${heading}
+               </h3>
+               <div>
+                   ${date.toDateString()}
+               </div>
+               <div>
+                 ${content}
+               </div>
+           </div>
+       </body>
+     </html>
+     `;
+     return htmlTemplate;
+ }
+ 
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+ });
+ 
+ var pool = new Pool(config);
+ app.get('/test-db', function (req, res) {
+    // make a select request
+    // return a response with the results
+    pool.query('SELECT * FROM test', function (err, result) {
+       if (err) {
+           res.status(500).send(err.toString());
        } else {
            res.send(JSON.stringify(result.rows));
        }
