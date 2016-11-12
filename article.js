b0d2fc45a71b367d102a94ee99035ba6e67e0a04
VoraPranay <ppvora131@gmail.com>
2016-11-13 00:24:59 +0530

    [imad-console] Updates article.js

--- article.js
+++ article.js
@@ -1,102 +1,74 @@
+body {
+    font-family: sans-serif;
+    background-color: lightgrey;
+    margin-top: 75px;
+}
 
-// Eg: coco98.imad.hasura-app.io/articles/article-one will result in article-one
- var currentArticleTitle = window.location.pathname.split('/')[2];
- 
- function loadCommentForm () {
-     var commentFormHtml = `
-         <h5>Submit a comment</h5>
-         <textarea id="comment_text" rows="5" cols="100" placeholder="Enter your comment here..."></textarea>
-         <br/>
-         <input type="submit" id="submit" value="Submit" />
-         <br/>
-         `;
-     document.getElementById('comment_form').innerHTML = commentFormHtml;
-     
-     // Submit username/password to login
-     var submit = document.getElementById('submit');
-     submit.onclick = function () {
-         // Create a request object
-         var request = new XMLHttpRequest();
-         
-         // Capture the response and store it in a variable
-         request.onreadystatechange = function () {
-           if (request.readyState === XMLHttpRequest.DONE) {
-                 // Take some action
-                 if (request.status === 200) {
-                     // clear the form & reload all the comments
-                     document.getElementById('comment_text').value = '';
-                     loadComments();    
-                 } else {
-                     alert('Error! Could not submit comment');
-                 }
-                 submit.value = 'Submit';
-           }
-         };
-         
-         // Make the request
-         var comment = document.getElementById('comment_text').value;
-         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
-         request.setRequestHeader('Content-Type', 'application/json');
-         request.send(JSON.stringify({comment: comment}));  
-         submit.value = 'Submitting...';
-         
-     };
+.center {
+    text-align: center;
+}
+
+.text-big {
+    font-size: 300%;
+}
+
+.bold {
+    font-weight: bold;
+}
+
+.img-medium {
+    height: 200px;
+}
+
+
+body {
+    background-color:#bf8040;
+    color:#ffefaa;
+    font-family:Serif,Arial;
+}
+#Cakes{
+padding:10px;
+   background-color:#996633; 
+}
+table {
+background-color:#bf8040;
+border-collapse:collapse;
+border-color:#996633;
+}
+#Header{
+   background-color:#996633; 
+   padding:10px;
+}
+#ContactUs{
+   background-color:#996633;
+   padding:10px;
+   position:relative ;
+}
+#Copyright{
+font-family:Arial;
+   background-color:#996633; 
+   padding:1px;
+}.container {
+     max-width: 800px;
+     margin: 0 auto;
+     color: #4a4a4a;
+     font-family: sans-serif;
+     padding-left: 20px;
+     padding-right: 20px;
  }
  
- function loadLogin () {
-     // Check if the user is already logged in
-     var request = new XMLHttpRequest();
-     request.onreadystatechange = function () {
-         if (request.readyState === XMLHttpRequest.DONE) {
-             if (request.status === 200) {
-                 loadCommentForm(this.responseText);
-             }
-         }
-     };
-     
-     request.open('GET', '/check-login', true);
-     request.send(null);
- }
-  
-  function escapeHTML (text)
-  {
-    var $text = document.createTextNode(html);
-  var $text = document.createTextNode(text);
-      var $div = document.createElement('div');
-      $div.appendChild($text);
-      return $div.innerHTML;
+ .comment {
+     margin-left: 10px;
+     padding-bottom: 10px;
+     border-bottom: 1px solid #ccc;
  }
  
- function loadComments () {
-         // Check if the user is already logged in
-     var request = new XMLHttpRequest();
-     request.onreadystatechange = function () {
-         if (request.readyState === XMLHttpRequest.DONE) {
-             var comments = document.getElementById('comments');
-             if (request.status === 200) {
-                 var content = '';
-                 var commentsData = JSON.parse(this.responseText);
-                 for (var i=0; i< commentsData.length; i++) {
-                     var time = new Date(commentsData[i].timestamp);
-                     content += `<div class="comment">
-                         <p>${escapeHTML(commentsData[i].comment)}</p>
-                         <div class="commenter">
-                             ${commentsData[i].username} - ${time.toLocaleTimeString()} on ${time.toLocaleDateString()} 
-                         </div>
-                     </div>`;
-                 }
-                 comments.innerHTML = content;
-             } else {
-                 comments.innerHTML('Oops! Could not load comments!');
-             }
-         }
-     };
-     
-     request.open('GET', '/get-comments/' + currentArticleTitle, true);
-     request.send(null);
- }
- 
- 
- // The first thing to do is to check if the user is logged in!
- loadLogin();
- loadComments();
+  .commenter {
+      font-style: italic;
+      font-size: 0.8em;
+} 
+}
+.comment_form {
+    margin-left: 10px;
+    padding-bottom: 10px;    border-bottom: 1px solid #ccc;
+ +}
