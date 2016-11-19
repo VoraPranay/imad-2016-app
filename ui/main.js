84f634a10e47db813fd73f9f2c8d70f32042fec8
VoraPranay <ppvora131@gmail.com>
2016-11-19 13:41:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,86 +1,23 @@
-<!doctype html>
-<html>
-    <head>
-        <link href="/ui/style.css" rel="stylesheet" />
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-<link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
-    </head>
-    <body>
-        <p><button class="w3-btn w3-orange w3-xlarge">HOME<i class="w3-margin-left fa fa-home"></i></button></p>
-       <li><a href="PROFILE.html"><span>profile</span></a></li>
-       
-        <div class="center">
-            <img src="/Users/prashant/Desktop/ima.jpg" class="img-medium"/>
-        </div>
-        <br>
-        <div class="center text- bold">
-           <h1> Hi ! I am pranay vora
-        
-            welcome to my first web page</h1>
-            
-        </div>
-       
-
-        <title>TheCakeShop</title>
-    
-      <span class="glyphicon glyphicon-thumbs-up"></span> Like
-  
-<div id=Header><h1 align="center">Chilling Me Softly</h1>
+// Counter code
+ var button = document.getElementById('counter');
+ button.onclick = function () {
  
- <p align="center"><i>The Worlds #1 cake shop.</i></p>   </div>    <hr>
- 
- <div id="Cakes"><p align="center"><strong>Cakes</strong></p>
- <br>
-<table align="center" border="5">
-<tr>
-<td>Victoria</td>
-<td>Battenberg</td>
-<td>Chocolate</td>
-</tr>
-<tr>
-<td>£7.99</td>
-<td>£3.99</td>
-<td>£6.99</td>
-</tr>
-<tr>
-<td>Cheesecake</td>
-<td>Carrot</td>
-<td>Red Velvet</td>
-</tr>
-<tr>
-<td>£7.99</td>
-<td>£8.99</td>
-<td>£8.69</td>
-</tr>
-<tr>
-<td>Lemon Drizzle</td>
-<td>Vanilla Cupcake X4</td>
-<td>Chocolate Cupcake X4</td>
-</tr>
-<tr>
-<td>£9.69</td>
-<td>£6.99</td>
-<td>£6.99</td>
-</tr>
-</table>
-<p align="center"><small>Visit shop for more!</small></p></div>
-<hr>
-<br>
-<div id=ContactUs align="center"><img height=160 width=280 src=" http://www.yogiscakes.co.uk/img/chocolate-slice.png"/>  
- <hr/>
-           <div class="footer">
-                   <button id="counter">  <span class="glyphicon glyphicon-thumbs-up"></span> Like </button>  <span id="count">0</span> times.
-                 <br/>
-                 <hr/>
-                 </div>
-<script type="text/javascript" src="/ui/main.js">
-         </script>
-<p align="center"><small><a href="3.html"><span>contact</span></a>    at   thecakeshop.net@gmail.com(Fake)</small></p></div>
-
-<div id=Copyright><p align="right"><small><small>Copyright&copy; TheCakeShop 2016(Fake)</small></small></p></div>
-
-</body>
-</html> 
+     // Create a request object
+     var request = new XMLHttpRequest();
+     
+     // Capture the response and store it in a variable
+     request.onreadystatechange = function () {
+       if (request.readyState === XMLHttpRequest.DONE) {
+           // Take some action
+           if (request.status === 200) {
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();          
+           }
+       }  
+       // Not done yet
+     };
+     
+     // Make the request
+     request.open('GET', 'http://VoraPranay.imad.hasura-app.io/counter', true);
+     request.send(null);
