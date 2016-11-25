e51d59b2805435ad27f94ad7caee934e036fd4d1
VoraPranay <ppvora131@gmail.com>
2016-11-26 01:59:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,27 @@
+/ Counter code
+ var button = document.getElementById('counter');
+ button.onclick = function () {
+ 
+     // Create a request object
+     var request = new XMLHttpRequest();
+     
+     // Capture the response and store it in a variable
+     request.onreadystatechange = function () {
+       if (request.readyState === XMLHttpRequest.DONE) {
+           // Take some action
+           if (request.status === 200) {
+               var counter = request.responseText;
+               var span = document.getElementById('count');
+               span.innerHTML = counter.toString();          
+           }
+       }  
+       // Not done yet
+     };
+ 
+     // Make the request
+     request.open('GET', 'http://VoraPranay.imad.hasura-app.io/counter', true);
+     request.send(null);}
+     
 function loadLoginForm () {
     var loginHtml = `
         <h3>Login/Register to unlock awesome features</h3>
