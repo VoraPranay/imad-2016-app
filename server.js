8cd7aaaad5557819e6efb0fc819e8e93b1dec268
VoraPranay <ppvora131@gmail.com>
2016-11-26 00:54:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,6 +70,11 @@ function createTemplate (data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'PROFILE.html'));
+});
+
+
 
 
 function hash (input, salt) {
