5f5ab1b0d2eacd29da7c325a1d6c1cb70b5eed79
VoraPranay <ppvora131@gmail.com>
2016-11-26 02:10:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,26 +1,28 @@
 // Counter code
- var button = document.getElementById('counter');
- button.onclick = function () {
- 
-     // Create a request object
-     var request = new XMLHttpRequest();
-     
-     // Capture the response and store it in a variable
-     request.onreadystatechange = function () {
-       if (request.readyState === XMLHttpRequest.DONE) {
-           // Take some action
-           if (request.status === 200) {
-               var counter = request.responseText;
-               var span = document.getElementById('count');
-               span.innerHTML = counter.toString();          
-           }
-       }  
-       // Not done yet
-     };
- 
-     // Make the request
-     request.open('GET', 'http://VoraPranay.imad.hasura-app.io/counter', true);
-     request.send(null);}
+var button = document.getElementById('counter');
+
+button.onclick = function () {
+
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+          // Take some action
+          if (request.status === 200) {
+              var counter = request.responseText;
+              var span = document.getElementById('count');
+              span.innerHTML = counter.toString();          
+          }
+      }  
+      // Not done yet
+    };
+    
+    // Make the request
+    request.open('GET', 'http://coco98.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
      
 function loadLoginForm () {
     var loginHtml = `
