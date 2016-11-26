3e47672a4ec04beb04e1608c7609b1f09eaaec7f
VoraPranay <ppvora131@gmail.com>
2016-11-26 16:48:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,6 +76,12 @@ app.get('/', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'home.html'));
 });
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'fireworks.html'));
+});
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'windows.html'));
+});
 var counter = 0;
 app.get('/counter', function (req, res) {
    counter = counter + 1;
